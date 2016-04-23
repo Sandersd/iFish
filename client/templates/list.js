4d0b793a9bc0f5b2a0da34f90b79316e15b52a48
Ethan Slattery <Ethan.Slattery@yahoo.com>
2016-04-23 15:50:25 -0700

    stash work

--- client/templates/list.js
+++
@@ -1,9 +0,0 @@
-Template.list.onCreated(function() {
-  Meteor.subscribe('fishes');
-});
-
-Template.list.helpers({
-  fishes: function(){
-    return Fishes.find();
-  }
-});
---
+++ client/templates/view_trips.js
@@ -0,0 +1,71 @@
+Template.view_trips.onCreated(function() {
+  Meteor.subscribe('trips');
+});
+
+Template.view_trips.helpers({
+  trips: function(){
+    return Trips.find();
+  }
+});
+
+Template.trip.helpers({
+  catches: function(){
+	  return Hauls.find();
+  }
+});
+
+Template.catch.helpers({
+
+  species: function(id){
+    return Hauls.findOne(id).species;
+  },
+
+  weight: function(id){
+    return Hauls.findOne(id).weight;
+  },
+
+  count: function(id){
+    return Hauls.findOne(id).count;
+  },
+
+  date: function(id){
+    return Hauls.findOne(id).date;
+  },
+
+  location: function(id){
+    return Hauls.findOne(id).location;
+  },
+
+  gear: function(id){
+    return Hauls.findOne(id).gear;
+  },
+
+  weather: function(id){
+    return Hauls.findOne(id).weather;
+  },
+
+  surfaceTemp: function(id){
+    return Hauls.findOne(id).surfaceTemp;
+  },
+
+  sold: function(id){
+    return Hauls.findOne(id).sold;
+  },
+
+  price: function(id){
+    return Hauls.findOne(id).price;
+  },
+
+  condition: function(id){
+    return Hauls.findOne(id).condition;
+  },
+
+  ticketType: function(id){
+    return Hauls.findOne(id).ticketType;
+  },
+
+  ticketNumber: function(id){
+    return Hauls.findOne(id).ticketNumber;
+  },
+
+});
